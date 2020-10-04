839eec635a02b3c32987c850b7a016f54daafc9a
Satantra Tewari <satantra1508@gmail.com>
2020-10-04 16:16:03 +0530

    [satantra] contact book created

---
+++ Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookCode
+{
+    class Contact
+    {
+        public string firstName, lastName, address, city, state, zipCode, phoneNo, eMail;
+        public Contact()
+        {
+            firstName = "";
+            lastName = "";
+            address = "";
+            city = "";
+            state = "";
+            zipCode = "";
+            phoneNo = "";
+            eMail = "";
+        }
+        public Contact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string eMail)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNo = phoneNo;
+            this.eMail = eMail;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace AddressBookCode
     {
         static void Main(string[] args)
         {
+            string firstName, lastName, address, city, state, zipCode, phoneNo, eMail;
             Console.WriteLine(" Welcome to Address Book Program!!! ");
+            Console.WriteLine(" Enter the following details in order");
+            Console.WriteLine(" 1. First Name\n 2. Last Name\n 3. Address\n 4. City\n 5. State\n 6. Zip Code\n 7. Phone Number\n 8. Email-ID\n ");
+            firstName = Console.ReadLine();
+            lastName = Console.ReadLine();
+            address = Console.ReadLine();
+            city = Console.ReadLine();
+            state = Console.ReadLine();
+            zipCode = Console.ReadLine();
+            phoneNo = Console.ReadLine();
+            eMail = Console.ReadLine();
+            Contact contact = new Contact(firstName, lastName, address, city, state, zipCode, phoneNo, eMail);
         }
     }
 }
