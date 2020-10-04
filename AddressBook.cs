73f39e65b18ea9b8b38bc5260861a1076e0a00c2
Satantra Tewari <satantra1508@gmail.com>
2020-10-04 16:55:56 +0530

    [satantra] contact added

---
+++ AddressBook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace AddressBookCode
+{
+    class AddressBook
+    {
+        List<Contact> list = new List<Contact>();
+        public void InputValues()
+        {
+            string[] details;
+            Console.WriteLine(" Enter the following details in order seperated by ,");
+            Console.WriteLine(" 1. First Name\n 2. Last Name\n 3. Address\n 4. City\n 5. State\n 6. Zip Code\n 7. Phone Number\n 8. Email ID\n ");
+            details = Console.ReadLine().Split(",");
+            Contact contact1 = new Contact(details[0], details[1], details[2], details[3], details[4], details[5], details[6], details[7]);
+            list.Add(contact1);
+        }
+        public void Display()
+        {
+            foreach(Contact contact1 in list)
+            {
+                Console.WriteLine(" FIRST NAME: " + contact1.firstName);
+                Console.WriteLine(" LAST NAME: " + contact1.lastName);
+                Console.WriteLine(" ADDRESS: " + contact1.address);
+                Console.WriteLine(" CITY: " + contact1.city);
+                Console.WriteLine(" STATE: " + contact1.state);
+                Console.WriteLine(" ZIP CODE: " + contact1.zipCode);
+                Console.WriteLine(" PHONE NUMBER: " + contact1.phoneNo);
+                Console.WriteLine(" EMAIL ID: " + contact1.eMail);
+            }
+        }
+        public int addContact(string firstName)
+        {
+            int c = 1;
+            foreach(Contact i in list)
+            {
+                if (i.firstName == firstName)
+                {
+                    Console.WriteLine(" Enter New Details as follows seperated by ,");
+                    Console.WriteLine(" Last Name\n Address\n City\n State\n Zip Code\n Phone Number\n Email ID\n");
+                    string[] details = Console.ReadLine().Split(",");
+                    i.lastName = details[0];
+                    i.address = details[1];
+                    i.city = details[2];
+                    i.state = details[3];
+                    i.zipCode = details[4];
+                    i.phoneNo = details[5];
+                    i.eMail = details[6];
+                }
+                else
+                    c = 0;
+            }
+            return c;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBookCode
 {
@@ -6,19 +7,34 @@ namespace AddressBookCode
     {
         static void Main(string[] args)
         {
-            string firstName, lastName, address, city, state, zipCode, phoneNo, eMail;
+            AddressBook addressBook = new AddressBook();
             Console.WriteLine(" Welcome to Address Book Program!!! ");
-            Console.WriteLine(" Enter the following details in order");
-            Console.WriteLine(" 1. First Name\n 2. Last Name\n 3. Address\n 4. City\n 5. State\n 6. Zip Code\n 7. Phone Number\n 8. Email-ID\n ");
-            firstName = Console.ReadLine();
-            lastName = Console.ReadLine();
-            address = Console.ReadLine();
-            city = Console.ReadLine();
-            state = Console.ReadLine();
-            zipCode = Console.ReadLine();
-            phoneNo = Console.ReadLine();
-            eMail = Console.ReadLine();
-            Contact contact = new Contact(firstName, lastName, address, city, state, zipCode, phoneNo, eMail);
+            int num = 1;
+            while(num==1)
+            {
+                List<Contact> list = new List<Contact>();
+                Console.WriteLine(" Enter your choice:\n 0. Add Data\n 1. View Data\n 2. Edit Data\n");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 0:
+                        addressBook.InputValues();
+                        break;
+                    case 1:
+                        addressBook.Display();
+                        break;
+                    case 2:
+                        Console.WriteLine(" Enter first name of contact you want to edit ");
+                        string firstName = Console.ReadLine();
+                        int checkName = addressBook.addContact(firstName);
+                        if (checkName == 0)
+                            Console.WriteLine(" Error. Name not found ");
+                        break;
+                    default:
+                        Console.WriteLine(" Please Enter correct choice 0, 1 or 2"); ;
+                        break;
+                }
+            }
         }
     }
 }
